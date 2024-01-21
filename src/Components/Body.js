94b8e066f301e9cbce4aa7388529aa3fa8bfad9a
Kartik Narang <narangk007@gmail.com>
2024-01-21 18:26:06 +0530

    Episode 5 - Lets Get Hooked

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,25 @@
 import { resArray } from "../utils/restaurantData.js";
 import { RestaurantCard } from "./RestaurantCard.js";
+import {useState} from "react";
 export const Body = () => {
-    return (
-      <div className="body">
-        <div className="res-container">
-          {resArray.map((resObj) => (
-            <RestaurantCard key={resObj.info.id} resObj={resObj} />
-          ))}
-        </div>
+  const [filteredArray, setFilteredArray] = useState(resArray);
+  return (
+    <div className="body">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          //Filter Logic
+          const newFilteredArray =  resArray.filter((res) => res.info.avgRating > 4);
+          setFilteredArray(newFilteredArray);
+        }}
+      >
+        Top Rated Restaraunts
+      </button>
+      <div className="res-container">
+        {filteredArray.map((resObj) => (
+          <RestaurantCard key={resObj.info.id} resObj={resObj} />
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
